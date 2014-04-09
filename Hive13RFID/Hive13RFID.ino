4fae1c5039d76e3680200894c3612237864f9be9
reportingsjr <reportingsjr@gmail.com>
2014-04-09 09:37:11 -0400

    Added user/badge caching to the door controller. Partly as a backup for the server, partly to speed up entries.

--- Hive13RFID/Hive13RFID.ino
+++ Hive13RFID/Hive13RFID.ino
@@ -30,6 +30,9 @@ char server[] = "172.16.3.233";
 IPAddress ip(172,16,3,230);
 EthernetClient client;
 
+// Cache of badge numbers that have succesfully opened the door.
+unsigned long usersCache[100];
+
 void setup() {
   Serial.begin(57600);
   // Let's set up the Ethernet Connections
@@ -70,6 +73,11 @@ void setup() {
   pinMode(6, OUTPUT);    
   // Initialize Wiegand Interface
   wg.begin();
+  
+  // initialize the usersCache array as all 0.
+  for(int i=0; i<100; i++) {
+    usersCache[i] = 0;
+  }
 }
 
 void loop() {
@@ -77,6 +85,9 @@ void loop() {
   int sensorValue = 0;    // Used to determine value of the magnetic sensor.
   int openCount = 0;      // Used to send to the controller the number of times the door's been opened.
   int readCount = 0;      // Used to send to the controller the number of times we've read a valid card.
+  unsigned long badgeID = 0;
+  boolean cached = false;
+  
   digitalWrite(13, HIGH);
   digitalWrite(6, HIGH);
   digitalWrite(12, HIGH);
@@ -96,9 +107,40 @@ void loop() {
     Serial.print(readCount);
     Serial.println();
     // if valid, open...
-
-    String bPath = "/doorcheck";
-    bPath = bPath + wg.getCode(),HEX;
+    
+    badgeID = wg.getCode();
+    // Check to see if the user has been cached
+    for(int i = 0; i < 100; i++) {
+      if(usersCache[i] == badgeID) {
+        // If it has been cached open the door.
+        Serial.println("Verified via cache ok to open door...");
+        cached = true;
+        digitalWrite(6, LOW);
+        delay(350);
+        digitalWrite(13, LOW);
+        digitalWrite(12, LOW);
+        int count=0;
+        while (count < 50) {
+          sensorValue = analogRead(A0);
+          digitalWrite(13, HIGH);
+          digitalWrite(12, HIGH);
+          delay(100);
+          digitalWrite(13, LOW);
+          digitalWrite(12, LOW);
+          delay(100);
+          if(analogRead(A0) != sensorValue){
+            break;
+          }
+          count++;  
+        }
+      }
+    }
+    String bPath = "/doorcheck/";
+    bPath = bPath + badgeID,HEX;
+    bPath = bPath + "/"
+    bPath = bPath + readCount;
+    bPath = bPath + "/";
+    bPath = bPath + openCount;
     bPath = bPath + "/go";
     EthernetClient c;
     HttpClient http(c);
@@ -111,28 +153,50 @@ void loop() {
       if (err >= 0) {
         Serial.print("Badge receievd status code: ");
         Serial.println(err);
-        if (err == 200) {
-           // Door Response is OK.  Open the door... 
-           Serial.println("Ok to open door...");
-           digitalWrite(6, LOW);
-           delay(350);
-           digitalWrite(13, LOW);
-           digitalWrite(12, LOW);
-           int count=0;
-           while (count < 50) {
-             sensorValue = analogRead(A0);
-             digitalWrite(13, HIGH);
-             digitalWrite(12, HIGH);
-             delay(100);
+        if (err == 200) {     
+           // We only need to open the door here if it wasn't opened from checking the cache.       
+           if(cached == false) {
+             // Door Response is OK.  Open the door...
+             Serial.println("Ok to open door...");
+             digitalWrite(6, LOW);
+             delay(350);
              digitalWrite(13, LOW);
              digitalWrite(12, LOW);
-             delay(100);
-             if(analogRead(A0) != sensorValue){
-               break;
+             int count=0;
+             while (count < 50) {
+               sensorValue = analogRead(A0);
+               digitalWrite(13, HIGH);
+               digitalWrite(12, HIGH);
+               delay(100);
+               digitalWrite(13, LOW);
+               digitalWrite(12, LOW);
+               delay(100);
+               if(analogRead(A0) != sensorValue){
+                 break;
                }
-             count++;  
+               count++;  
+             }
+             // user is allowed in but hasn't been cached, add them to the cache.
+             Serial.println("Adding user to cache.");
+             for(int i = 0; i < 100; i++) {
+               // loop until we find a spot that is still zero and stick the badge id there.
+               if(usersCache[i] == 0) {
+                 usersCache[i] = badgeID;
+                 break;
+               }
              }
            }
+         }
+         else {
+           if(cached == true) {
+             // remove them from the cached list as they are not allowed in
+             for(int i = 0; i < 100; i++) {
+               if(usersCache[i] == badgeID) {
+                 usersCache[i] = 0;
+               }
+             }
+           }
+         }
       } 
       else {
         Serial.println("Badge FAILED to get response headers.");
@@ -145,7 +209,8 @@ void loop() {
   else {
   // 
   }
-
+  badgeID = 0;
+  cached = false;
 }
 
 
