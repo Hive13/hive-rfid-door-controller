b514c8d8d1242506daaf7e7d426c4e86438a029b
Greg J Arnold <garnold@cc-sw.com>
2015-11-24 11:52:14 -0500

    Add log source.

---
+++ hive_vending_machine/log.h
@@ -0,0 +1,22 @@
+#ifndef __LOG_H
+#define __LOG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if 1
+#define log_begin() _log_begin()
+#define log_msg(x, ...) _log_msg(x, #__VA_ARGS__)
+#else
+#define log_begin()
+#define log_msg(x, ...)
+#endif
+
+void _log_begin(void);
+void _log_msg(char *format, ...);
+#ifdef __cplusplus
+};
+#endif
+
+#endif /* __LOG_H */
