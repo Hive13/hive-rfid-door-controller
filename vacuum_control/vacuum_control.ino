947f4e051ac8b111719f9dedbe6f762a2594bce2
Greg J Arnold <greg@arnoldassociates.com>
2016-10-04 22:32:07 -0400

    Add in the web server.

--- vacuum_control/vacuum_control.ino
+++ vacuum_control/vacuum_control.ino
@@ -1,5 +1,11 @@
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 
+#include "cJSON.h"
+
+static unsigned char vacuum_state = LOW, vacuum_act = LOW;
 char ssid[] = "hive13int";
 char pass[] = "hive13int";
 int status = WL_IDLE_STATUS;
@@ -7,13 +13,28 @@ int status = WL_IDLE_STATUS;
 int on_button  = D6;
 int off_button = D5;
 int vacuum     = D1;
+ESP8266WebServer server(80);
 #define TIMEOUT 5000
+
+void turn_on(void)
+	{
+	vacuum_state = HIGH;
+	server.send(200, "text/plain", "Commanded on.");
+	}
+
+void turn_off(void)
+	{
+	vacuum_state = LOW;
+	server.send(200, "text/plain", "Commanded off.");
+	}
 
 void loop(void)
 	{
-	static unsigned char vacuum_state = LOW, vacuum_act = LOW;
 	static unsigned long last_time = 0;
 	unsigned long m = millis();
+
+	server.handleClient();
+	
 	if (digitalRead(on_button) == LOW)
 		{
 		vacuum_state = HIGH;
@@ -34,6 +55,10 @@ void loop(void)
 
 void setup(void)
 	{
+	pinMode(on_button, INPUT_PULLUP);
+	pinMode(off_button, INPUT_PULLUP);
+	pinMode(vacuum, OUTPUT);
+	digitalWrite(vacuum, LOW);
 	Serial.begin(115200);
 	delay(1);
 
@@ -49,11 +74,18 @@ void setup(void)
 	Serial.print("You're connected to the network");
 	printCurrentNet();
 	printWifiData();
-	pinMode(on_button, INPUT_PULLUP);
-	pinMode(off_button, INPUT_PULLUP);
-	pinMode(vacuum, OUTPUT);
-	digitalWrite(vacuum, LOW);
 
+	server.on("/", handleRoot);
+	server.on("/on", turn_on);
+	server.on("/off", turn_off);
+	//server.on ( "/test.svg", drawGraph );
+	server.on ("/inline", []()
+		{
+		server.send(200, "text/plain", "this works as well");
+		});
+	server.onNotFound ( handleNotFound );
+	server.begin();
+	Serial.println("HTTP server started");
 	}
 
 void printWifiData()
@@ -92,4 +124,40 @@ void printCurrentNet()
 	long rssi = WiFi.RSSI();
 	Serial.print("signal strength (RSSI):");
 	Serial.println(rssi);
+	}
+
+
+void handleRoot()
+	{
+	struct cJSON *root = cJSON_CreateObject(), *a;
+	char *c;
+
+	a = cJSON_CreateNumber(millis());
+	cJSON_AddItemToObject(root, "uptime", a);
+	a = cJSON_CreateBool(vacuum_state);
+	cJSON_AddItemToObject(root, "commanded_state", a);
+	a = cJSON_CreateBool(vacuum_act);
+	cJSON_AddItemToObject(root, "actual_state", a);
+
+	c = cJSON_Print(root);
+	server.send(200, "text/json", c);
+	free(c);
+	}
+
+void handleNotFound()
+	{
+	unsigned char i;
+	String message = "File Not Found\n\n";
+	message += "URI: ";
+	message += server.uri();
+	message += "\nMethod: ";
+	message += (server.method() == HTTP_GET) ? "GET" : "POST";
+	message += "\nArguments: ";
+	message += server.args();
+	message += "\n";
+	
+	for (i = 0; i < server.args(); i++)
+		message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+	
+	server.send(404, "text/plain", message);
 	}
