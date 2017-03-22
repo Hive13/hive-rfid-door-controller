1676a7d24025f4c504e90a26274ad194d67eaf9d
Greg J Arnold <greg@arnoldassociates.com>
2017-03-21 20:33:50 -0400

    Move code to a library.

--- Hive13RFID/Hive13RFID.ino
+++ Hive13RFID/Hive13RFID.ino
@@ -7,12 +7,16 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-#define SHA512_SZ 64
+#include "cJSON.h"
+#include "API.h"
+
 #define BODY_SZ 1024
 
+#define NETWORK_TIMEOUT 5000
+#define NETWORK_DELAY   25
+
 char *location   = "main_door";
-char key[]       = {'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'};
-static char *hex = "0123456789ABCDEF";
+char key[]       = {'Y', 'o', 'u', ' ', 'l', 'o', 's', 't', ' ', 't', 'h', 'e', 'G', 'a', 'm', 'e'};
 
 // I'm using the Wiegand library to make reading the files easier. 
 // https://github.com/monkeyboard/Wiegand-Protocol-Library-for-Arduino.git
@@ -23,32 +27,24 @@ static char *hex = "0123456789ABCDEF";
 
 char check_badge(unsigned long badge)
 	{
-	static unsigned long scan_count = 0, l;
-	char sha_buf[SHA512_SZ], body[BODY_SZ], out[BODY_SZ];
-	unsigned char i;
+	static unsigned long scan_count = 0;
+	char body[BODY_SZ], *out;
 	EthernetClient ec;
 	HttpClient hc(ec);
-	SHA512 sha;
-	int err;
+	int err, body_len;
+	struct cJSON *resp, *cs;
+	unsigned long start = millis(), l;
+	unsigned char rv[2 * sizeof(unsigned long)];
+	unsigned char rc;
 
-	l = snprintf(body, BODY_SZ, "{\"badge\":%lu,\"http\":true,\"item\":\"%s\",\"random\":[%lu,%lu],\"version\":1}",
-		badge, location, millis(), scan_count++);
-	
-	sha.reset();
-	sha.update(key, sizeof(key));
-	sha.update(body, l);
-	sha.finalize(sha_buf, sha.hashSize());
+	memcpy(rv, &start, sizeof(unsigned long));
+	memcpy(rv + sizeof(unsigned long), &scan_count, sizeof(unsigned long));
+	scan_count++;
 
-	l = snprintf(out, BODY_SZ, "{\"data\":%s,\"device\":\"%s\",\"checksum\":\"", body, location);
-	
-	for (i = 0; i < SHA512_SZ; i++)
-		{
-		out[l++] = hex[((sha_buf[i] & 0xF0) >> 4)];
-		out[l++] = hex[(sha_buf[i] & 0x0F)];
-		}
-
-	out[l++] = '\"';
-	out[l++] = '}';
+	out = get_request(badge, location, location, key, sizeof(key), rv, sizeof(rv));
+	l = strlen(out);
+	Serial.print("Sending ");
+	Serial.println(out);
 
 	hc.beginRequest();
   hc.post("intweb.at.hive13.org", "/api/access");
@@ -59,8 +55,49 @@ char check_badge(unsigned long badge)
 	hc.flush();
 	hc.write(out, l);
 	hc.flush();
+	free(out);
+
 	err = hc.responseStatusCode();
-	return err == 200;
+	if (err != 200)
+		return RESPONSE_BAD_HTTP;
+	body_len = hc.contentLength();
+	if (body_len + 1 > BODY_SZ)
+		return RESPONSE_BAD_HTTP;
+	
+	if (hc.skipResponseHeaders() > 0)
+		{
+		Serial.println("Header error.");
+		return RESPONSE_BAD_HTTP;
+		}
+	
+	start = millis();
+	l = 0;
+	while ((hc.connected() || hc.available()) && ((millis() - start) < NETWORK_TIMEOUT))
+		{
+		if (hc.available())
+			{
+			body[l++] = hc.read();
+			body_len--;
+			start = millis();
+			}
+		else
+			delay(NETWORK_DELAY);
+		}
+	body[l++] = 0;
+	hc.stop();
+	Serial.print("Body: ");
+	Serial.println(body);
+
+	rc = parse_response(body, &resp, key, sizeof(key), rv, sizeof(rv));
+
+	if (!(cs = cJSON_GetObjectItem(resp, "access")) || cs->type != cJSON_True)
+		{
+		cJSON_Delete(resp);
+		return RESPONSE_ACCESS_DENIED;
+		}
+
+	cJSON_Delete(resp);
+	return RESPONSE_GOOD;
 	}
 
 void open_door(void)
@@ -95,8 +132,11 @@ void open_door(void)
 
 
 WIEGAND wg;
-byte mac[] = { 0x48, 0x49, 0x56, 0x45, 0x31, 0x33};
-IPAddress ip(172,16,3,230);
+byte mac[]     = { 0x48, 0x49, 0x56, 0x45, 0x31, 0x33};
+byte ip[]      = {172, 16, 3, 245};
+byte gateway[] = {172, 16, 2, 1};
+byte subnet[]  = {255, 255, 254, 0};
+byte dns_d[]   = {172, 16, 2, 1};
 EthernetClient client;
 
 // Cache of badge numbers that have succesfully opened the door.
@@ -115,11 +155,7 @@ void setup()
 	Serial.begin(57600);
 
   Serial.println("Initializing Ethernet Controller.");
-	while (Ethernet.begin(mac) != 1)
-		{
-		Serial.println("Error obtaining DHCP address.  Let's wait a second and try again");
-		delay(1000);
-		}
+	Ethernet.begin(mac, ip, dns_d, gateway, subnet);
 
   // Initialize Wiegand Interface
   wg.begin();
@@ -143,7 +179,7 @@ void setup()
 
 void loop()
 	{
-	char badge_ok;
+	char badge_code;
 	unsigned long badgeID = 0;
 	unsigned long time = 0;
 	boolean cached = false;
@@ -179,11 +215,11 @@ void loop()
 			}
 		}
 
-	badge_ok = check_badge(badgeID);
-	Serial.print("Badge OK: ");
-	Serial.println(badge_ok, DEC);
+	badge_code = check_badge(badgeID);
+	Serial.print("Badge code: ");
+	Serial.println(badge_code, DEC);
 
-	if (badge_ok)
+	if (badge_code == RESPONSE_GOOD)
 		{
 		// We only need to open the door here if it wasn't opened from checking the cache.
 		if (!cached)
@@ -193,7 +229,6 @@ void loop()
 			Serial.println("Adding user to cache.");
 			for (i = 0; i < (sizeof(usersCache) / sizeof(usersCache[0])); i++)
 				{
-				// loop until we find a spot that is still zero and stick the badge id there.
 				if (usersCache[i] == 0)
 					{
 					usersCache[i] = badgeID;
