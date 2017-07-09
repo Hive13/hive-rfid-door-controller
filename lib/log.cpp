220bbdbd1863117c3808a70b7da65b1a039b48b3
Greg J Arnold <greg@arnoldassociates.com>
2017-07-09 15:05:23 -0400

    Add in progress logging (to be improved).

--- lib/log.cpp
+++ lib/log.cpp
@@ -24,3 +24,45 @@ void _log_msg(unsigned short line, char *file, char *format, ...)
 
 	Serial.println(message);
 	}
+
+void _log_progress_start(unsigned short line, char *file, char *format, ...)
+	{
+	char message[1024];
+	va_list vargs;
+	int i;
+
+	i = snprintf(message, sizeof(message), "%s:%hu - ", file, line);
+
+	if (i < 0)
+		return;
+
+	va_start(vargs, format);
+	vsnprintf(message + i, sizeof(message) - i, format, vargs);
+	va_end(vargs);
+
+	Serial.print(message);
+	}
+
+void _log_progress_end(unsigned short line, char *file, char *format, ...)
+	{
+	char message[1024];
+	va_list vargs;
+
+	va_start(vargs, format);
+	vsnprintf(message, sizeof(message), format, vargs);
+	va_end(vargs);
+
+	Serial.println(message);
+	}
+
+void _log_progress(unsigned short line, char *file, char *format, ...)
+	{
+	char message[1024];
+	va_list vargs;
+
+	va_start(vargs, format);
+	vsnprintf(message, sizeof(message), format, vargs);
+	va_end(vargs);
+
+	Serial.print(message);
+	}
--- lib/log.h
+++ lib/log.h
@@ -6,15 +6,24 @@ extern "C" {
 #endif
 
 #if 1
-#define log_begin(x) _log_begin(x)
-#define log_msg(x, ...) _log_msg(__LINE__, __FILE__, x, ##__VA_ARGS__)
+#define log_begin(x)               _log_begin(x)
+#define log_msg(x, ...)            _log_msg(__LINE__, __FILE__, x, ##__VA_ARGS__)
+#define log_progress_start(x, ...) _log_progress_start(__LINE__, __FILE__, x, ##__VA_ARGS__)
+#define log_progress_end(x, ...)   _log_progress_end(__LINE__, __FILE__, x, ##__VA_ARGS__)
+#define log_progress(x, ...)       _log_progress(__LINE__, __FILE__, x, ##__VA_ARGS__)
 #else
 #define log_begin()
 #define log_msg(x, ...)
+#define log_progress_start(x, ...)
+#define log_progress_end(x, ...)
+#define log_progress(x, ...)
 #endif
 
 void _log_begin(unsigned long baud);
 void _log_msg(unsigned short line, char *file, char *format, ...);
+void _log_progress_start(unsigned short line, char *file, char *format, ...);
+void _log_progress_end(unsigned short line, char *file, char *format, ...);
+void _log_progress(unsigned short line, char *file, char *format, ...);
 #ifdef __cplusplus
 };
 #endif
