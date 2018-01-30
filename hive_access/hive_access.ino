aec9b7f88f5c5e23897206263a17e311db158b2e
Greg J Arnold <greg@arnoldassociates.com>
2018-01-30 18:15:50 -0500

    Add in some start-of-day beep patterns.

--- hive_access/hive_access.ino
+++ hive_access/hive_access.ino
@@ -14,22 +14,58 @@ static WIEGAND wg;
 
 struct beep_pattern start_of_day =
 	{
-	.beep_ms     = 200,
-	.silence_ms  = 100,
+	.beep_ms     = 400,
+	.silence_ms  = 150,
 	.cycle_count = 2,
 	.options     = RED_WITH_BEEP,
+	};
+struct beep_pattern init_wg =
+	{
+	.beep_ms     = 100,
+	.silence_ms  = 100,
+	.cycle_count = 2,
+	.options     = RED_ALWAYS,
+	};
+struct beep_pattern init_log =
+	{
+	.beep_ms     = 100,
+	.silence_ms  = 100,
+	.cycle_count = 3,
+	.options     = RED_ALWAYS,
+	};
+struct beep_pattern init_temp =
+	{
+	.beep_ms     = 100,
+	.silence_ms  = 100,
+	.cycle_count = 4,
+	.options     = RED_ALWAYS,
+	};
+struct beep_pattern init_wifi =
+	{
+	.beep_ms     = 100,
+	.silence_ms  = 100,
+	.cycle_count = 5,
+	.options     = RED_ALWAYS,
 	};
 
 void setup(void)
 	{
 	unsigned char i = LOW;
 	ui_init();
-
+	
+	beep_it(&init_wg);
 	wg.begin(D0_PIN, D0_PIN, D1_PIN, D1_PIN);
+	delay(1000);
+	beep_it(&init_log);
 	log_begin(115200);
+	delay(1000);
 
+	beep_it(&init_temp);
 	access_temperature_init();
+	delay(1000);
+	beep_it(&init_wifi);
 	wifi_init();
+	delay(1000);
 
 	beep_it(&start_of_day);
 	log_msg("Ready to rumble!");
