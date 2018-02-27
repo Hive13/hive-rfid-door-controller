dd37dce4490ec2c26bac4e58574fc318eb9d0ebe
Greg J Arnold <greg@arnoldassociates.com>
2018-02-27 16:34:28 -0500

    Add in Arduino version of random and HTTP request code.

--- lib/http.cpp
+++ lib/http.cpp
@@ -1,18 +1,28 @@
+#include "config.h"
+
 #include <Wiegand.h>
+#ifdef PLATFORM_ARDUINO
+#include <Ethernet.h> 
+#include <b64.h>
+#include <HttpClient.h>
+#else
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266HTTPClient.h>
+#endif
 
 #include "http.h"
 #include "cJSON.h"
 #include "API.h"
-#include "temp.h"
 #include "schedule.h"
 #include "log.h"
 #include "ui.h"
+
+#ifndef PLATFORM_ARDUINO
 #include "wifi.h"
 
 #define RANDOM_REG32  ESP8266_DREG(0x20E44)
+#endif
 
 struct beep_pattern invalid_card =
 	{
@@ -39,8 +49,99 @@ struct beep_pattern packet_error =
 static char *location    = LOCATION;
 static char *device      = DEVICE;
 static char key[]        = KEY;
-static const char host[] = HTTP_HOST;
 static char nonce[33];
+
+#ifdef PLATFORM_ARDUINO
+#define RAND_SIZE (2 * sizeof(unsigned long))
+#define BODY_SZ 1024
+
+unsigned char http_request(unsigned char *request, struct cJSON **result, char *rand, unsigned char rand_len)
+	{
+	char body[BODY_SZ];
+	EthernetClient ec;
+	HttpClient hc(ec);
+	int err, body_len;
+	struct cJSON *new_nonce;
+	unsigned long start, l;
+	unsigned char i;
+	
+	l = strlen(request);
+	log_msg("Request: %s", request);
+
+	hc.beginRequest();
+	hc.post("intweb.at.hive13.org", "/api/access");
+	hc.flush();
+	hc.sendHeader("Content-Type", "application/json");
+	hc.flush();
+	hc.sendHeader("Content-Length", l);
+	hc.flush();
+	hc.write(request, l);
+	hc.flush();
+	free(request);
+
+	err = hc.responseStatusCode();
+	if (err != 200)
+		return RESPONSE_BAD_HTTP;
+	body_len = hc.contentLength();
+	if (body_len + 1 > BODY_SZ)
+		return RESPONSE_BAD_HTTP;
+
+	if (hc.skipResponseHeaders() > 0)
+		{
+		Serial.println("Header error.");
+		return RESPONSE_BAD_HTTP;
+		}
+
+	start = millis();
+	l = 0;
+	while ((hc.connected() || hc.available()) && ((millis() - start) < NETWORK_TIMEOUT))
+		{
+		if (hc.available())
+			{
+			body[l++] = hc.read();
+			body_len--;
+			start = millis();
+			}
+		else
+			delay(NETWORK_DELAY);
+		}
+	body[l++] = 0;
+	hc.stop();
+	log_msg("Response: %s", body);
+
+	i = parse_response(body, result, key, sizeof(key), rand, rand_len);
+	if (i == RESPONSE_GOOD)
+		{
+		new_nonce = cJSON_GetObjectItem(*result, "new_nonce");
+		memmove(nonce, new_nonce->valuestring, 32);
+		nonce[32] = 0;
+		log_msg("Good response!");
+		}
+	else if (i == RESPONSE_BAD_NONCE)
+		{
+		log_msg("Invalid nonce.");
+		update_nonce();
+		}
+	else
+		{
+		log_msg("Error: %i", i);
+		beep_it(&packet_error);
+		}
+	return i;
+	}
+
+static unsigned long scan_count = 0;
+void get_rand(char *rand)
+	{
+	unsigned long m = millis();
+	memcpy(rand, &m, sizeof(unsigned long));
+	memcpy(rand + sizeof(unsigned long), &scan_count, sizeof(unsigned long));
+	scan_count++;
+	}
+
+#endif
+#ifdef PLATFORM_ESP
+static const char host[] = HTTP_HOST;
 
 unsigned char http_request(unsigned char *request, struct cJSON **result, char *rand, unsigned char rand_len)
 	{
@@ -89,23 +190,32 @@ unsigned char http_request(unsigned char *request, struct cJSON **result, char *
 	return i;
 	}
 
-unsigned char check_badge(unsigned long badge_num, void (*success)(void))
+#define RAND_SIZE 16
+
+void get_rand(char *rand)
 	{
-	struct cJSON *json, *result;
 	unsigned char i;
 	unsigned long r;
-	unsigned char *out;
-	char rand[16];
-
+	
 	for (i = 0; i < sizeof(rand); i++)
 		{
 		if (!(i % 4))
 			r = RANDOM_REG32;
 		rand[i] = ((r >> (3 - i)) & 0xFF);
 		}
+	}
+#endif
+
+unsigned char check_badge(unsigned long badge_num, void (*success)(void))
+	{
+	struct cJSON *json, *result;
+	unsigned char i;
+	unsigned long r;
+	unsigned char *out;
+	char rand[RAND_SIZE];
 	
+	get_rand(rand);
 	out = (unsigned char *)get_request(badge_num, "access", location, device, key, sizeof(key), rand, sizeof(rand), nonce);
-	
 	i = http_request(out, &result, rand, sizeof(rand));
 
 	if (i == RESPONSE_GOOD)
@@ -133,21 +243,14 @@ void log_temp(unsigned long temp)
 	{
 	struct cJSON *json, *result;
 	unsigned char i;
-	unsigned long r;
 	unsigned char *out;
-	char rand[16];
+	char rand[RAND_SIZE];
 
-	for (i = 0; i < sizeof(rand); i++)
-		{
-		if (!(i % 4))
-			r = RANDOM_REG32;
-		rand[i] = ((r >> (3 - i)) & 0xFF);
-		}
-	
 	json = cJSON_CreateObject();
 	cJSON_AddItemToObjectCS(json, "item", cJSON_CreateString("annex"));
 	cJSON_AddItemToObjectCS(json, "temperature", cJSON_CreateNumber(temp));
-
+	
+	get_rand(rand);
 	out = (unsigned char *)log_data(json, device, key, sizeof(key), rand, sizeof(rand), nonce);
 	i = http_request(out, &result, rand, sizeof(rand));
 
@@ -164,17 +267,10 @@ void update_nonce(void)
 	{
 	struct cJSON *result;
 	unsigned char i;
-	unsigned long r;
 	unsigned char *out;
-	char rand[16];
+	char rand[RAND_SIZE];
 	
-	for (i = 0; i < sizeof(rand); i++)
-		{
-		if (!(i % 4))
-			r = RANDOM_REG32;
-		rand[i] = ((r >> (3 - i)) & 0xFF);
-		}
-	
+	get_rand(rand);
 	out = (unsigned char *)get_nonce(device, key, sizeof(key), rand, sizeof(rand));
 	i = http_request(out, &result, rand, sizeof(rand));
 
