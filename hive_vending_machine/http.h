dc81c304b03c6d16fe2bef31dcb36829b3da17ab
Greg J Arnold <greg@arnoldassociates.com>
2018-03-01 11:58:48 -0500

    Symlink http.[ch].

--- hive_vending_machine/http.h
+++ hive_vending_machine/http.h
@@ -1,15 +1,1 @@
-#ifndef __HTTP_H
-#define __HTTP_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-signed int http_get_json(char *host, char *path, char *req_body, char **resp);
-signed int http_get(char *req, char *host, char *path);
-
-#ifdef __cplusplus
-};
-#endif
-
-#endif /* __HTTP_H */
+../lib/http.h
