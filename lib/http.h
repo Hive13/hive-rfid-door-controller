c36301dde0750b25614583994f66d92d5679eb96
Greg J Arnold <greg@arnoldassociates.com>
2018-04-03 17:19:27 -0400

    Need extern C here.

--- lib/http.h
+++ lib/http.h
@@ -3,9 +3,17 @@
 
 #include "config.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 unsigned char check_badge(unsigned long badge_num, void (*success)(void));
 void log_temp(unsigned long temp, char *name);
 void update_nonce(void);
 unsigned char can_vend(unsigned long badge);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* __HTTP_H */
